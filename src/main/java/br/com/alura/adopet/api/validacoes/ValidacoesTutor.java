da53e35eb475f543e656ed050d6e5e629e8561a3
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-11-08 15:53:19 -0300

    Padrão Strategy e Chain of Responsibility

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacoesTutor.java
@@ -0,0 +1,35 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ValidacoesTutor implements ValidacaoSolicitacaoAdocao {
+    @Autowired
+    private AdocaoRepository adocaoRepository;
+
+    public void validar(SolicitacaoAdocaoDto dto) {
+        List<Adocao> adocoes = adocaoRepository.findAll();
+        for (Adocao a : adocoes) {
+            if (a.getTutor().getId().equals(dto.idTutor()) && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
+            }
+        }
+        for (Adocao a : adocoes) {
+            int contador = 0;
+            if (a.getTutor().getId().equals(dto.idTutor()) && a.getStatus() == StatusAdocao.APROVADO) {
+                contador = contador + 1;
+            }
+            if (contador == 5) {
+                throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
+            }
+        }
+    }
+}
